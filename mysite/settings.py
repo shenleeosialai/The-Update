e769c54074d93d035a0f05df565c53393558574a
shen <shenleekhalid@gmail.com>
2026-03-09 21:52:13 +0300

    logo update v 1.0.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
 
     # Third-party apps
     "rest_framework",
-    # "rest_framework.authtoken",
+    "rest_framework.authtoken",
     "dj_rest_auth",
     "allauth",
     "allauth.account",
